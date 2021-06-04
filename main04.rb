16ec770246572ad2918222ad8bb2348d7840d576
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 11:26:37 +0430

    First Commit

---
+++ main04.rb
@@ -0,0 +1,32 @@
+# a = 5
+# b = 20
+# c = a + b
+# puts c
+
+# print "========================="
+
+# a = "Tehran"
+# b = "Rasht"
+# c = "300km"
+# d= " The distance between " + a + " & " + b + " is " + c
+# puts d
+
+# print "========================="
+
+# c = "300km"
+# d= " The distance between Tehran & Rasht is #{c} km. "
+# puts d
+
+# $name = "Ali"
+#
+# def ret_name
+#   puts $name
+# end
+#
+# ret_name
+
+# print "==========================="
+
+# $name = "Ali"
+# $name = "Reza"
+# puts $name
